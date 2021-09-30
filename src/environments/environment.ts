40caafe01bb221838cdde908f2642c585c444041
Agustin Gaitan <gosagustingaitan@gmail.com>
2021-09-30 11:15:40 -0300

    3 parte

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyDifYzZxAoHaprE3q2jvur6rvlqp8YoL5c",
-    authDomain: "modelopaciallabolv.firebaseapp.com",
-    projectId: "modelopaciallabolv",
-    storageBucket: "modelopaciallabolv.appspot.com",
-    messagingSenderId: "21551472071",
-    appId: "1:21551472071:web:0f6886bd2b8ba766290de2"
-  }
 };
 
 /*
